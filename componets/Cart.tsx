31f811dd152f95817740699b25c7ad767672b25f
Dean Chap <cchapotarong@mun.ca>
2024-05-28 18:47:56 -0600

    cart fix

--- componets/Cart.tsx
+++ componets/Cart.tsx
@@ -1,56 +1,56 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
-import Button from "./Button";
+// import { Box, Typography } from "@mui/material";
+// import React from "react";
+// import Button from "./Button";
 
-const Cart = () => {
-  return (
-    <Box
-      sx={{
-        postion: "fixed",
-        height: "100vh",
-        width: "100%",
-        backgroundColor: "rgba(128, 128, 128, 0.459)",
-      }}
-    >
-      <Box
-        sx={{
-          position: "absolute",
-          right: 150,
-          minHeight: "20vh",
-          width: "25vw",
-          backgroundColor: "white",
-          color: "black",
-          padding: "1.5rem",
-          textTransform: "uppercase",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography>Cart</Typography>
-          <Typography>Remove All</Typography>
-        </Box>
-        <Box>PRODUCTS</Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography>Total</Typography>
-          <Typography>$$$</Typography>
-        </Box>
-        <Button color="#d97d45" variant="contained" sx={{ width: "100%" }}>
-          Checkout
-        </Button>
-      </Box>
-    </Box>
-  );
-};
+// const Cart = () => {
+//   return (
+//     <Box
+//       sx={{
+//         postion: "fixed",
+//         height: "100vh",
+//         width: "100%",
+//         backgroundColor: "rgba(128, 128, 128, 0.459)",
+//       }}
+//     >
+//       <Box
+//         sx={{
+//           position: "absolute",
+//           right: 150,
+//           minHeight: "20vh",
+//           width: "25vw",
+//           backgroundColor: "white",
+//           color: "black",
+//           padding: "1.5rem",
+//           textTransform: "uppercase",
+//           display: "flex",
+//           flexDirection: "column",
+//         }}
+//       >
+//         <Box
+//           sx={{
+//             display: "flex",
+//             justifyContent: "space-between",
+//           }}
+//         >
+//           <Typography>Cart</Typography>
+//           <Typography>Remove All</Typography>
+//         </Box>
+//         <Box>PRODUCTS</Box>
+//         <Box
+//           sx={{
+//             display: "flex",
+//             justifyContent: "space-between",
+//           }}
+//         >
+//           <Typography>Total</Typography>
+//           <Typography>$$$</Typography>
+//         </Box>
+//         <Button color="#d97d45" variant="contained" sx={{ width: "100%" }}>
+//           Checkout
+//         </Button>
+//       </Box>
+//     </Box>
+//   );
+// };
 
-export default Cart;
+// export default Cart;
