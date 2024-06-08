a1a403c10c6191ff86340ca418dc1c38ee24d936
Dean Chap <cchapotarong@mun.ca>
2024-06-07 19:50:21 -0600

    Product section commit

--- pages/product.tsx
+++ pages/product.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Nav from '../componets/Nav'
-import { Box } from '@mui/material'
-import Product from '../componets/Product'
+// import React from 'react'
+// import Nav from '../componets/Nav'
+// import { Box } from '@mui/material'
+// import Product from '../componets/Product'
 
-const product = () => {
-  return (
-    <Box>
-        <Nav />
-        <Product />
-    </Box>
-  )
-}
+// const product = () => {
+//   return (
+//     <Box>
+//         <Nav />
+//         <Product />
+//     </Box>
+//   )
+// }
 
-export default product
+// export default product
