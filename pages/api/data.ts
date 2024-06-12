ae230205dde2c178ee2fc8841bc58b229daa7369
Dean Chap <cchapotarong@mun.ca>
2024-06-11 21:04:33 -0600

    api data commit

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,15 +1,15 @@
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from 'next';
-// import data from "../../data/data.json";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import data from "../../data/data.json";
 
 
-// type Data = {
-//   name: string
-// }
+type Data = {
+  name: string
+}
 
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<any>
-// ) {
-//   res.status(200).json(data)
-// }
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  res.status(200).json(data)
+}
